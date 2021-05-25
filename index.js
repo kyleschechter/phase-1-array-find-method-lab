493579bfc74db6ecdd881e3c118fde31c66b6148
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-05-25 12:11:42 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(array) {
+    const winYear = array.find(array => array.result === 'W')
+    if (winYear === undefined) {
+        return undefined
+    } else {
+        return winYear.year
+    }
+}
